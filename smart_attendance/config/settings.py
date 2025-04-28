a99034ee96d82a7e9cb1727cb6e424935037d572
moahmedmedhat <157950299+moahmedmedhat@users.noreply.github.com>
2025-04-28 21:55:58 +0300

    Update settings.py

--- smart_attendance/config/settings.py
+++ smart_attendance/config/settings.py
@@ -1,1 +1,8 @@
-API_URL = "http://localhost:5000"
+# Backend API URL (Change this to your actual backend URL)
+BROKER_URL = "9713f3d043bf4e95bc9ef27e29b4654e.s1.eu.hivemq.cloud"
+BROKER_PORT = 8883
+USERNAME = "smart_attednace_system"
+PASSWORD = "Smart#12345"
+
+train_path="/home/pi5/smart_attendance/core/working_dataset"
+model_path="/home/pi5/smart_attendance/core/lbph_model (4).xml"
