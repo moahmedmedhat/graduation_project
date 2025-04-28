0d65147ede696a196418cd71b08899726c6b824d
moahmedmedhat <157950299+moahmedmedhat@users.noreply.github.com>
2025-04-28 21:54:53 +0300

    Update api_client.py

--- smart_attendance/backend/api_client.py
+++ smart_attendance/backend/api_client.py
@@ -4,10 +4,10 @@ import paho.mqtt.client as mqtt
 import ssl
 import json
 # HiveMQ Cloud credentials
-BROKER_URL = "9713f3d043bf4e95bc9ef27e29b4654e.s1.eu.hivemq.cloud"
+BROKER_URL = "broker_url"
 BROKER_PORT = 8883
-USERNAME = "smart_attednace_system"
-PASSWORD = "Smart#12345"
+USERNAME = "username"
+PASSWORD = "password"
 
 def start_listening(device_id):
     topic = f"devices/{device_id}/control"
